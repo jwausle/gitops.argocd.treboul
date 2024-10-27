6b059e7ec72d88ba37860ee7e908eafda294e6a1
Jan Winter <winter@jwausle>
2024-10-27 13:45:15 +0100

    Try to deploy first application

--- infrastructure/cluster/k3s-cluster/deploy-argocd.sh
+++ infrastructure/cluster/k3s-cluster/deploy-argocd.sh
@@ -21,26 +21,43 @@ if [ ! -d "$ARGOCD_RELEASE_CRDS_DIR" ]; then
   exit 2
 fi
 
-kubectl create namespace $ARGOCD_RELEASE_NAMESPACE
+HELMCHART_ONLY=false
+if [[ "$*" =~ "--helm-only" ]]; then
+  HELMCHART_ONLY=true
+fi
 
-# Install ArgoCD crds (only) - with a unclear workaround
-# - Unclear why it is not working with ...
-# - kubectl apply -k https://github.com/argoproj/argo-cd/manifests/crds\?ref\=$ARGOCD_APP_VERSION
-# - Then the argocd-applicationset-controller and argocd-server not starting (CrashLoopBackOff)
-kubectl apply -k "$ARGOCD_RELEASE_CRDS_DIR"
-kubectl delete ClusterRoleBindings --selector 'app.kubernetes.io/part-of=argocd'
-kubectl delete ClusterRoles --selector 'app.kubernetes.io/part-of=argocd'
-kubectl delete namespace $ARGOCD_RELEASE_NAMESPACE
+install-argocd-helm() {
+  helm repo add argocd-repo https://argoproj.github.io/argo-helm
+  helm upgrade --install $ARGOCD_RELEASE_NAME \
+       argocd-repo/argo-cd \
+       --namespace $ARGOCD_RELEASE_NAMESPACE --create-namespace \
+       --version $ARGOCD_VERSION \
+       --skip-crds \
+       -f "$ARGOCD_DIR"/helm-values.yaml
+}
 
-# Install ArgoCD
-helm repo add argocd-repo https://argoproj.github.io/argo-helm
-helm upgrade --install $ARGOCD_RELEASE_NAME \
-     argocd-repo/argo-cd \
-     --namespace $ARGOCD_RELEASE_NAMESPACE --create-namespace \
-     --version $ARGOCD_VERSION \
-     --skip-crds \
-     -f "$ARGOCD_DIR"/helm-values.yaml
+install-argocd-crds() {
+  kubectl create namespace $ARGOCD_RELEASE_NAMESPACE
 
-echo
-# Customize ArgoCD route `argocd.treboul.localhost`
-kubectl apply -f "$ARGOCD_DIR"/ingressroute.yaml --namespace $ARGOCD_RELEASE_NAMESPACE
+  # Install ArgoCD crds (only) - with a unclear workaround
+  # - Unclear why it is not working with ...
+  # - kubectl apply -k https://github.com/argoproj/argo-cd/manifests/crds\?ref\=$ARGOCD_APP_VERSION
+  # - Then the argocd-applicationset-controller and argocd-server not starting (CrashLoopBackOff)
+  kubectl apply -k "$ARGOCD_RELEASE_CRDS_DIR"
+  kubectl delete ClusterRoleBindings --selector 'app.kubernetes.io/part-of=argocd'
+  kubectl delete ClusterRoles --selector 'app.kubernetes.io/part-of=argocd'
+  kubectl delete namespace $ARGOCD_RELEASE_NAMESPACE
+}
+
+if [ "$HELMCHART_ONLY" = true ]; then
+  install-argocd-helm
+else
+  # Install ArgoCD crds
+  install-argocd-crds
+  # Install ArgoCD
+  install-argocd-helm
+  echo
+  # Customize ArgoCD route `argocd.treboul.localhost`
+  kubectl apply -f "$ARGOCD_DIR"/ingressroute.yaml --namespace $ARGOCD_RELEASE_NAMESPACE
+fi
+
