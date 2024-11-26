f0d66ccf4537c5c058d123d4525115e477c9da1f
Jan Winter <winter@jwausle>
2024-11-26 21:39:47 +0100

    Enhance docker run helm-repository and docker-registry

--- infrastructure/cluster/docker-registry/docker-run.sh
+++ infrastructure/cluster/docker-registry/docker-run.sh
@@ -1,19 +1,26 @@
 SCRIPT_DIR=$(dirname "$0")
 SCRIPT_PATH="$( cd -- "$SCRIPT_DIR" >/dev/null 2>&1 ; pwd -P )"
 
-CONTAINER_NAME="registry"
+CONTAINER_NAME="docker-registry"
+CONTAINER_IMAGE="registry:2"
+CONTAINER_VOLUME="docker-registry"
+CONTAINER_PORT=5001
 
 if docker stats --no-stream $CONTAINER_NAME; then
   echo "Docker $CONTAINER_NAME is already running"
   exit 0
 fi
 
+if ! docker volume ls | grep -q $CONTAINER_VOLUME; then
+  docker volume create $CONTAINER_VOLUME
+fi
+
 docker run -d --rm \
-    -p 5001:5000 \
+    -p $CONTAINER_PORT:5000 \
     --name $CONTAINER_NAME \
-    -v "$SCRIPT_PATH"/data:/var/lib/registry \
+    -v "$CONTAINER_VOLUME":/var/lib/registry \
     -v "$SCRIPT_PATH"/auth:/auth \
     -e "REGISTRY_AUTH=htpasswd" \
     -e "REGISTRY_AUTH_HTPASSWD_REALM=Registry Realm" \
     -e REGISTRY_AUTH_HTPASSWD_PATH=/auth/htpasswd \
-    registry:2
+    $CONTAINER_IMAGE
--- infrastructure/cluster/helm-repository/docker-run.sh
+++ infrastructure/cluster/helm-repository/docker-run.sh
@@ -1,21 +1,30 @@
 SCRIPT_DIR=$(dirname "$0")
 SCRIPT_PATH="$( cd -- "$SCRIPT_DIR" >/dev/null 2>&1 ; pwd -P )"
 
-CONTAINER_NAME="chartmuseum"
+CONTAINER_NAME="helm-repository"
+CONTAINER_IMAGE="ghcr.io/helm/chartmuseum:v0.16.2"
+CONTAINER_VOLUME="helm-repository"
+CONTAINER_PORT=5002
+USERNAME="admin"
+PASSWORD="admin"
 
 if docker stats --no-stream $CONTAINER_NAME; then
   echo "Helm repository $CONTAINER_NAME is already running"
   exit 0
 fi
 
+if ! docker volume ls | grep -q $CONTAINER_VOLUME; then
+  docker volume create $CONTAINER_VOLUME
+fi
+
 docker run --rm -d \
     --name $CONTAINER_NAME \
-    -p 5002:8080 \
+    -p $CONTAINER_PORT:8080 \
     -e DEBUG=1 \
     -e STORAGE=local \
     -e STORAGE_LOCAL_ROOTDIR=/charts \
-    -v "$SCRIPT_PATH"/data:/charts \
-    ghcr.io/helm/chartmuseum:v0.16.2
-    --basic-auth-user=admin \
-    --basic-auth-pass=admin \
+    -v "$CONTAINER_VOLUME":/charts \
+    $CONTAINER_IMAGE \
+    --basic-auth-user=$USERNAME \
+    --basic-auth-pass=$PASSWORD \
     --auth-anonymous-get
